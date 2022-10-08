c827adeba323615a0d65328fddf658f42a66ad08
chimoski <chimaobiprince867@gmail.com>
2022-10-08 14:38:40 +0100

    .

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const navIcon = document.getElementById("nav-icon");
 const navBar = document.querySelector(".navbar");
+const ball = document.querySelector(".ball");
+const moon = document.querySelector(".fa-moon");
+const sun = document.querySelector(".fa-sun");
+const _switch = document.querySelector(".switch-container");
+const box = document.querySelector(".box");
+
 navIcon.addEventListener("click", () => {
 	navBar.classList.toggle("navBarMobile");
-	console.log(navBar.classList);
 	navIcon.classList.toggle("open");
 });
+
+// let ltheme = localStorage.getItem("tme");
+let theme = "light";
+
+_switch.addEventListener("click", () => {
+	if (theme === "light") {
+		sun.style.transform = "translateX(45px) rotate(250deg) ";
+		sun.style.opacity = "0";
+		moon.style.transform = "translateX(-45px) rotate(350deg)";
+		moon.style.opacity = "1";
+		theme = "dark";
+	} else {
+		sun.style.transform = "translateX(0)";
+		sun.style.opacity = "1";
+		moon.style.transform = "translateX(0)";
+		moon.style.opacity = "0";
+		theme = "light";
+	}
+	document.body.className = theme;
+});
+
+// setInterval(() => {
+// 	// localStorage.setItem("theme", theme);
+// 	// console.log(theme);
+// }, 500);
