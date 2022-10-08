4ed6e2c72013e86e1cce4f438ebfb3b669fe15c1
chimoski <chimaobiprince867@gmail.com>
2022-10-08 09:44:08 +0100

    added sticky navbar

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ navIcon.addEventListener("click", () => {
 	console.log(navBar.classList);
 	navIcon.classList.toggle("open");
 });
+
+let startProductBarPos = -1;
+window.onscroll = function () {
+	if (startProductBarPos < 0) startProductBarPos = findPosY(navBar);
+
+	if (pageYOffset > startProductBarPos) {
+		navBar.style.position = "fixed";
+		navBar.style.top = 0;
+	} else {
+		navBar.style.position = "relative";
+	}
+};
+
+function findPosY(obj) {
+	var curtop = 0;
+	if (typeof obj.offsetParent != "undefined" && obj.offsetParent) {
+		while (obj.offsetParent) {
+			curtop += obj.offsetTop;
+			obj = obj.offsetParent;
+		}
+		curtop += obj.offsetTop;
+	} else if (obj.y) curtop += obj.y;
+	return curtop;
+}
